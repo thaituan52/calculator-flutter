43e8fda113b1261dd82fddb2129f3a0202348505
dothaituan <tuandothai522004@gmail.com>
2025-06-09 16:47:16 +0700

    version 0.3
     fixing bug, able to work normally, may have some hidden bugs

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         title: 'Calculator',
         theme: ThemeData.dark(),
         home: MyCalculator(),
-        //debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false,
       ),
       );
   }
@@ -47,8 +47,8 @@ class CalculatorState extends ChangeNotifier {
     //del enable when the button pressed is not = 
     //and the display is not empty
     _displayVisible = true;
-    if (_previousDisplayVisible) {
-      _clearMem(); //Only clear the memory when the previous display is visible aka have calculated something, else we only care about the current number (_num1)
+    if (_previousDisplayVisible && buttonText != '=') {
+      _clearMem(); //Only clear the memory when the previous display is visible aka have calculated something, else we only care about the current number (num1)
     }
     if (buttonText == 'AC') {
       _clear(); //reset everything
@@ -56,18 +56,18 @@ class CalculatorState extends ChangeNotifier {
       //case when we have -x, it will trigger the - and turn into x
       if (!_num) {
         _display = _toggleSign(_display);
+        _num1 = double.parse(_display);
       } else {
         String sub = _format(_num1.toString()) + _operator;
         _display = sub + _toggleSign(_display.substring(sub.length));
-        // print("Num1: $_num1, Num2: $_num2, Operator: $_operator");
       }
     } else if (buttonText == '%') {
       if (!_num) {
         _display = _percentage(_display);
+        _num1 = double.parse(_display);
       } else {
         String sub = _format(_num1.toString()) + _operator;
         _display = sub + _percentage(_display.substring(sub.length));
-        // print("Num1: $_num1, Num2: $_num2, Operator: $_operator");
       }
     } else if (['+', '-', '×', '÷'].contains(buttonText)) {
       _setOperator(buttonText);
@@ -104,7 +104,7 @@ class CalculatorState extends ChangeNotifier {
     _previousDisplayVisible = false;
     _displayVisible = false;
     _num = false;
-    notifyListeners();
+    //notifyListeners();
   }
 
   void _clearMem() {
@@ -134,6 +134,7 @@ class CalculatorState extends ChangeNotifier {
   //can turn the num and operator into a list (future features)
   void _setOperator(String op) {
     String numString = '';
+
   if (_display.isNotEmpty && ['+', '-', '×', '÷'].contains(_display[_display.length - 1]) && op == '-') {
     // The last character of _display is an minus operator, we want to check if there is an operator (that is not minus)
     //before it, if so, we keep that, else ignore it
@@ -141,34 +142,38 @@ class CalculatorState extends ChangeNotifier {
     _num1 = double.parse(numString);
     if (_display[_display.length - 2] == '-') {
       // If the previous operator is also a minus, we ignore it
-      return;
     } else {
       // Otherwise, we keep the previous operator and add the new one
-      _display = _display + op;
+      numString = _display;
+      //_operator = op; dont change operator here
+      _display = numString + op;
+      //_calculated = false;
+      _num = true;
+      notifyListeners();   
       return;
     }
   } else if (_display.isNotEmpty && ['+', '-', '×', '÷'].contains(_display[_display.length - 1])) {
     // The last character of _display is an operator, we want to change depend on it
     numString = _display.substring(0, _display.length - 1);
     _num1 = double.parse(numString);
-  } else if ((_display.contains('+') || _display.contains('-') || _display.contains('×') || _display.contains('÷')) && (_display[0] != '-')) {
-    //in case we have a second operator in the expression, first calculatre
-    //the first part of the expression then work with the second part
-    int opIndex = _display.indexOf(RegExp(r'[+\-×÷]'));
-    numString = _display.substring(0, opIndex);
-    _num1 = double.parse(numString);
-    _calculate();
-    numString = _display;
   } else {
-    //normal case
-    numString = _display;
-    _num1 = double.parse(numString);
+    int opIndex = _display[0] == '-' ? (_display.substring(1).indexOf(RegExp(r'[+\-×÷]'))) : _display.indexOf(RegExp(r'[+\-×÷]')); // If the first character is a minus, we ignore it
+    numString = opIndex == -1 ? _display : _display.substring(0, opIndex);
+    if (_num1 == 0) {
+      _num1 = double.parse(numString);
+    }
+    if (_display.substring(numString.length).length > 1) {
+      _num = true;
+      _calculate();
+      _previousDisplayVisible = false; //Done represent it on the previous display
+      _previousDisplay = '';
+      numString = _display;
+    }
   }
     _operator = op;
     _display = numString + _operator;
     //_calculated = false;
     _num = true;
-    //print("Num1: $_num1, Num2: $_num2, Operator: $_operator");	
     notifyListeners();                    
     
   }
@@ -181,22 +186,17 @@ class CalculatorState extends ChangeNotifier {
       String temp = _format(_num1.toString()) + _operator;
       _num2 = double.parse(_display.substring(temp.length));
     }
-
     _display = _format(_num1.toString()) + _operator + _format(_num2.toString());
-    //print("Num1: $_num1, Num2: $_num2, Operator: $_operator");
     double result = 0;
     
 
     switch (_operator) {
       case '+':
         result = _num1 + _num2;
-        break;
       case '-':
         result = _num1 - _num2;
-        break;
       case '×':
         result = _num1 * _num2;
-        break;
       case '÷':
         if (_num2 != 0) {
           result = _num1 / _num2;
@@ -204,7 +204,6 @@ class CalculatorState extends ChangeNotifier {
           _display = 'Error';
           return;
         }
-        break;
     }
     _previousDisplay = _display;
     _display = result.toString();
@@ -225,28 +224,34 @@ class CalculatorState extends ChangeNotifier {
     } else {
       _display += number;
     }
-    _previousDisplay = '';
-    _previousDisplayVisible = false;
+    if (!_num) {
+      _num1 = double.parse(_display);
+      //print("Num1: $_num1, Num2: $_num2, Operator: $_operator");
+    } //have to update _num1 
   }
 
   // also, the 64-bit double precision problem, still good at the moment 
   void _addDecimal() {
-    if (!_display[_display.length - 1].contains('.')) { // Check if the last character is not a dot
+    if (_display.isNotEmpty && !_display[_display.length - 1].contains('.')) { // Check if the last character is not a dot
       _display += '.';
+    } else if (_display.isEmpty) {
+      _display = '0.'; // If the display is empty, start with 0.
     }
     notifyListeners();
   }
 
   String _format(String number) {
     double value = double.parse(number);
-    if (value == value.toInt()) {
-      number = value.toInt().toString();
-    } else {
-      number = value.toString();
-    }
-    return number;
+  // Round to 2 decimal places
+  double rounded = double.parse(value.toStringAsFixed(8));
+  
+  // If it's a whole number, return as integer
+  if (rounded == rounded.toInt()) {
+    return rounded.toInt().toString();
   }
 
+  return rounded.toStringAsFixed(8).replaceAll(RegExp(r'0*$'), '');
+  }
 }
 
 class MyCalculator extends StatefulWidget {
@@ -267,6 +272,7 @@ class _CalculatorState extends State<MyCalculator> {
       case 0:
         page = CalculatorButtons();
       case 1:
+        // Future feature: Showing history of calculations
         page = Placeholder();
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -349,15 +355,18 @@ class CalculatorButtons extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: row.map((buttonText) {
               return Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(4.0),
+                child: AspectRatio(
+                  aspectRatio: 1,
                   child: ElevatedButton(
                     onPressed: () {
                       calculatorState._onButtonPressed(buttonText);
                     },
-                    child: Text(
-                      buttonText,
-                      style: TextStyle(fontSize: 24),
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        buttonText,
+                        style: TextStyle(fontSize: 24),
+                      ),
                     ),
                   ),
                 ),
